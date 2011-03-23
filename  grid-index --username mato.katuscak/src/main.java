88c78bb55fc5192fc0c583bcfcde18a5fd804c25
mato.katuscak <mato.katuscak@9740fc9a-9024-cb51-2cc0-d46b5475e33a>
2011-03-23 23:59:49 +0000

    Chceck the control of the overflow the buffer in Virtual Page and secure space for offset other realPages !

---  grid-index --username mato.katuscak/src/main.java
+++  grid-index --username mato.katuscak/src/main.java
@@ -20,13 +20,10 @@ public class main {
         System.out.println("HelloWorld");
         File f = new File("C:\\vstup.txt");
         File file = new File("C:\\test.index");
-        Point_Storage point = new Point_Storage(f);
-        //Point_Storage point = new Point_Storage(300, 2);
-        grid = new Index_Maker(2, 64, point);
-        //System.out.println(grid.median(1));
+        //Point_Storage point = new Point_Storage(f);
+        Point_Storage point = new Point_Storage(100, 4);
+        grid = new Index_Maker(4, 190, point);
         System.out.println(grid.pocetStranok());
-        grid.getRozdelenieIndexu_median();
-        System.out.println(grid.getRozdelenieIndexu_median());
         System.out.println(grid.getRozdelenieIndexu_pocetnost());
         System.out.println(grid.pocet_roznych_hodnot(0));
         System.out.println(grid.pocet_roznych_hodnot(1));
@@ -42,77 +39,24 @@ public class main {
 
         }
 
-        //index.add(grid.storage.getPoints().get(0));
-        //index.add(grid.storage.getPoints().get(2));
         index.close();
         index.open();
 
-        System.out.println("Pocet stranok"+ index.pocetStranok);
+        ArrayList<Double> ll = new ArrayList<Double>();
+        ll.add(.5);
+        ll.add(.5);
+        ll.add(.5);
+        ll.add(.5);
+
+        System.out.println("Vysledok vyhladavaia: " + index.hladaj_Rectangle(grid.getStorage().getPoints().get(0), ll));
+
+        System.out.println("Pocet stranok" + index.pocetStranok);
         for (Integer i : index.cache.platne_stranky) {
             Virtual_Page page = index.cache.loadPage(index, i);
             System.out.println(page.getObsah());
 
         }
-        ArrayList<Double> dlzky = new ArrayList<Double>();
-        dlzky.add(1.);
-        dlzky.add(1.);
-        index.hladaj_Rectangle(grid.getStorage().getPoints().get(9),dlzky);
         index.close();
-
-        //test na skusanie zapisu :)
-
-        //File subor = new File("C:/1.moja");
-
-
-        //write_toFile(subor);
-        // read_file(subor);
-
-
-    }
-
-    public static void read_file(File f) throws IOException {
-        RandomAccessFile raf = new RandomAccessFile(f, "rw");
-        FileChannel channel = raf.getChannel();
-        ByteBuffer baf = ByteBuffer.allocateDirect(grid.getVelkost_stranky());
-        channel.read(baf);
-        baf.rewind();
-        /*
-        for(int i = 0; i <5;i++){
-             int p = baf.getInt();
-            System.out.println(p);
-        }*/
-
-        for (int i = 0; i < grid.getPocet_objektov(); i++) {
-            ArrayList<Double> temp = new ArrayList<Double>();
-            int iD = baf.getInt();
-            for (int j = 0; j < grid.pocet_suradnic; j++) {
-                temp.add(baf.getDouble());
-
-            }
-
-            Point p = new Point(iD, temp);
-            System.out.println(p);
-        }
-        baf.clear();
-        channel.close();
-    }
-
-    public static void write_toFile(File f) throws IOException {
-        RandomAccessFile raf = new RandomAccessFile(f, "rw");
-        raf.setLength(grid.getVelkost_stranky() * grid.pocetStranok());
-        FileChannel channel = raf.getChannel();
-        System.out.println(channel.size());
-        ByteBuffer bb = ByteBuffer.allocateDirect(grid.getVelkost_stranky());
-        /*for (int i = 0; i < 5; i++){
-            bb.putInt(i);
-        }*/
-        for (int i = 0; i < grid.pocet_objektov; i++) {
-            grid.getStorage().getPoints().get(i).save(bb);
-        }
-        bb.rewind();
-        channel.write(bb);
-        channel.close();
-        raf.close();
 
     }
 }
