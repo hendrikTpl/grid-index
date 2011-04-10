7a2ec5e7efd9d2f3f2f1936ee39501355d3b9822
mato.katuscak <mato.katuscak@9740fc9a-9024-cb51-2cc0-d46b5475e33a>
2011-04-10 23:04:37 +0000

    Complete function algorithm :)

--- trunk/ grid-index --username mato.katuscak/src/main.java
+++ trunk/ grid-index --username mato.katuscak/src/main.java
@@ -17,12 +17,12 @@ public class main {
     public static void main(String[] args) throws Exception {
 
 //        System.out.println("HelloWorld");
-//        File f = new File("C:\\1000000_10_exp.txt");
+//        File f = new File("C:\\vstup.txt");
 //        File file = new File("C:\\test.index");
-        File index_file = new File("C:\\Testy\\1000000_5_gauss.idf");
-        //Point_Storage point = new Point_Storage(f);
-//        Point_Storage point = new Point_Storage(2000000, 10);
-//        grid = new Index_Maker(10, 1500, point);
+        File index_file = new File("C:\\Testy\\100000_10_un.idf");
+//        Point_Storage point = new Point_Storage(f);
+////        Point_Storage point = new Point_Storage(2000000, 10);
+//        grid = new Index_Maker(2, 100, point);
 //
 //        System.out.println(grid.pocetStranok());
 //
@@ -34,10 +34,10 @@ public class main {
 //
 //        Grid_index index = new Grid_index(grid, file);
 //        index.open();
-////        for (int i = 0; i < grid.pocet_objektov; i++) {
-////            //System.out.println(index.cislo_stranky(grid.storage.getPoints().get(i)));
-////            //System.out.println(grid.storage.getPoints().get(i));
-////        }
+//        for (int i = 0; i < grid.pocet_objektov; i++) {
+//            //System.out.println(index.cislo_stranky(grid.storage.getPoints().get(i)));
+//            //System.out.println(grid.storage.getPoints().get(i));
+//        }
 //        System.out.println("Pocet Objektov index1 " + grid.pocet_objektov);
 //        for (int i = 0; i < grid.pocet_objektov; i++) {
 //            index.add(grid.storage.getPoints().get(i));
@@ -77,9 +77,6 @@ public class main {
 //        pw2.close();
 
 
-        Grid_index index2 = Grid_index.serializeFrom(index_file);
-        index2.open();
-
 //        Generator.generateUniformDouble(100000,5,-3,5,new File("C:\\100000_5_un.txt"));
 //        Generator.generateUniformDouble(100000,10,-3,5,new File("C:\\100000_10_un.txt"));
 //        int[] body = new int[]{0};
@@ -90,28 +87,30 @@ public class main {
 //        Generator.generateExponentialDouble(1000000,10,hranice,1,1,new File("C:\\1000000_10_exp.txt"));
 //        Generator.generateExponentialDouble(100000,5,hranice,1,1,new File("C:\\100000_5_exp.txt"));
 //        Generator.generateExponentialDouble(100000,10,hranice,1,1,new File("C:\\100000_10_exp.txt"));
-        //System.out.println("Pocet realne v indexe " + pocet);
-        System.out.println("Pocet Objektov index2 " + index2.pocet_objektov);
-
-        index2.close();
+        Grid_index index2 = Grid_index.serializeFrom(index_file);
+//        index2.open();
+//        //System.out.println("Pocet realne v indexe " + pocet);
+//        System.out.println("Pocet Objektov index2 " + index2.pocet_objektov);
+//
+//        index2.close();
         index2.open();
+        System.out.println(index2.struktura);
 
 
         System.out.println("Pocet vs. stranok " + index2.pocetStranok);
 
-        ArrayList<Double> dlzky = new ArrayList<Double>();
-        dlzky.add(0.9);
-        dlzky.add(0.9);
-        dlzky.add(0.9);
-        dlzky.add(1.3);
-        dlzky.add(1.4);
+//        ArrayList<Double> dlzky = new ArrayList<Double>();
+//        dlzky.add(4.2);
+//        dlzky.add(4.2);
+//        dlzky.add(4.2);
+//        dlzky.add(4.2);
+//        dlzky.add(1.1);
 
-        ArrayList<Double> suradnice = new ArrayList<Double>();
-        suradnice.add(-1.);
-        suradnice.add(-1.);
-        suradnice.add(0.);
-        suradnice.add(0.);
-        suradnice.add(0.);
+
+        List<Double> suradnice = Arrays.asList(0., 0., 0., 0., 0., 0., 0., 0., 0., 0.);
+        List<Double> dlzky = Arrays.asList(3., 1., 3., 3., 2., 3., 2., 3., 3., 3.);
+
+
         Point p = new Point(1, suradnice);
 
         long start = System.currentTimeMillis();
